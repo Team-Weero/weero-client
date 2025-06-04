1a2c3ca1a143a20c492685965fdede948e261108
seeum0507 <kwonstella0507@gmail.com>
2025-06-04 22:23:45 +0900

    chore: add HomePage route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import "./index.css";
+import HomePage from "./page/HomePage";
 
 function App() {
   return (
     <Routes>
-      <Route />
+      <Route path="/homePage" element={<HomePage />} />
     </Routes>
   );
 }
