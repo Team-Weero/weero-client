f85ba8f4109f524e4ee5a6a014828b3c63af83d3
seeum0507 <kwonstella0507@gmail.com>
2025-06-04 22:33:30 +0900

    chore: HomePage파일 생성

---
+++ src/page/HomePage.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+import theme from "../page/theme";
+import NavBar from "../components/NavBar";
+
+const HomePage = () => {
+  return (
+    <>
+      <NavBar text="홈" />
+    </>
+  );
+};
+
+export default HomePage;
