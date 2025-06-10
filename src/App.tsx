c80b22e691e72d9c2522acd62ca5bbf02fd5d564
seeum0507 <kwonstella0507@gmail.com>
2025-06-10 20:01:12 +0900

    feat : Image 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import "./index.css";
 import HomePage from "./page/HomePage";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/homePage" element={<HomePage />} />
-    </Routes>
-  );
+  return <Routes></Routes>;
 }
 
 export default App;
