526a662ebd4959680f349cc33ab526e142b5aed7
Sohee Kim <ttohee1275@gmail.com>
2025-07-03 10:05:26 +0900

    feat: scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import WeeCommunity from "./page/community/WeeCommunity";
 import WeeDetail from "./page/community/WeeDetail";
 import WritePost from "./page/community/WritePost";
+import ScrollToTop from "./components/ScrollToTop";
+import Community from "./page/community/Community";
 
 function App() {
   return (
-    <Routes>
-      <Route path="wee-community" element={<WeeCommunity />} />
-      <Route path="wee-detail" element={<WeeDetail />} />
-      <Route path="write-post" element={<WritePost />} />
-    </Routes>
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="wee-community" element={<WeeCommunity />} />
+        <Route path="wee-detail" element={<WeeDetail />} />
+        <Route path="write-post" element={<WritePost />} />
+        <Route path="community" element={<Community />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
