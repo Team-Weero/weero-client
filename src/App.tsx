8c1bcddeccbc9e3c8b2650649922dee6b87276ba
seeum0507 <kwonstella0507@gmail.com>
2025-08-01 12:48:20 +0900

    feat : create ChatHome file

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import ScrollToTop from "./components/ScrollToTop";
 import Community from "./page/community/Community";
 import AlarmPage from "./page/alarm/AlarmPage";
 import HomePage from "./page/HomePage";
+import ChatHome from "./page/ChatHome";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/community" element={<Community />} />
         <Route path="/alarm" element={<AlarmPage />} />
         <Route path="/home-page" element={<HomePage />} />
+        <Route path="chat-home" element={<ChatHome />} />
       </Routes>
     </BrowserRouter>
   );
