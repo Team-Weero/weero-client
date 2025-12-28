36a37cc6d585a869796b36ac909797cc471607a7
김소희 <ttohee1275@gmail.com>
2025-12-28 21:41:31 +0900

    fix: router import dupe

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./index.css";
 import WeeCommunity from "./page/community/WeeCommunity";
 import WeeDetail from "./page/community/WeeDetail";
 import WritePost from "./page/community/WritePost";
 import ScrollToTop from "./components/ScrollToTop";
-import Community from "./page/community/Community";
 import AlarmPage from "./page/alarm/AlarmPage";
 import Login from "./Login";
 import Signup from "./Signup";
@@ -20,30 +19,39 @@ import ChatPage from "./page/ChatPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <ScrollToTop />
       <Routes>
         <Route path="/wee-community" element={<WeeCommunity />} />
         <Route path="/wee-detail" element={<WeeDetail />} />
         <Route path="/write-post" element={<WritePost />} />
-        <Route path="/community" element={<Community />} />
         <Route path="/alarm" element={<AlarmPage />} />
-        <Route path="/home-page" element={<HomePage />} />
+        {/* 홈 페이지는 이제부터 루트입니다. */}
+        <Route path="/" element={<HomePage />} />
         <Route path="counsel-apply" element={<CounselApply />} />
         <Route path="chat-home" element={<ChatHome />} />
         <Route path="/chat-page" element={<ChatPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/change" element={<ChangePwd />} />
-        <Route path="/mypage" element={<MyPage userName="김위로" userEmail="wee@dsm.hs.kr" />} />
+        <Route
+          path="/mypage"
+          element={<MyPage userName="김위로" userEmail="wee@dsm.hs.kr" />}
+        />
         <Route path="/myposts" element={<MyPosts />} />
         <Route path="/profile" element={<EditProfile />} />
         <Route
           path="/owner"
-          element={<OwnersPage userName="김위로" userEmail="wee@dsm.hs.kr" isPeerCounselor={true} />}
+          element={
+            <OwnersPage
+              userName="김위로"
+              userEmail="wee@dsm.hs.kr"
+              isPeerCounselor={true}
+            />
+          }
         />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { StrictMode } from "react";
 import { ThemeProvider } from "@emotion/react";
 import { theme } from "./style/theme.tsx";
 
